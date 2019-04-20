26d4f7c80c0aff2bfacf7afffa2d5206bea07171
Nikhil Komirisetti <nkomirisetti@gmail.com>
2019-04-20 12:10:07 -0400

    fixed method

--- app/src/main/java/com/example/studysmarter/dbLayer/DAL/DataAccessLayerHelper.java
+++ app/src/main/java/com/example/studysmarter/dbLayer/DAL/DataAccessLayerHelper.java
@@ -3,7 +3,9 @@ package com.example.studysmarter.dbLayer.DAL;
 import android.arch.persistence.room.Room;
 import android.content.Context;
 
+import com.example.studysmarter.dbLayer.dataBuilder.BuildCards;
 import com.example.studysmarter.dbLayer.database.CardsDatabase;
+import com.example.studysmarter.dbLayer.tables.Cards;
 import com.example.studysmarter.dbLayer.tables.Decks;
 
 import java.util.List;
@@ -11,9 +13,11 @@ import java.util.List;
 
 public class DataAccessLayerHelper {
     public static void insertCards(CardsDatabase cd, int deckID, List<String> terms, List<String> definitions) {
-        // find max index of deck
-        // create card objects with that starting index
-        // insert cards into deck
+        int startingIndex = cd.getCardDAO().getHighestCardID(deckID);
+        List<Cards> cardsList = BuildCards.createCards(deckID, terms, definitions, startingIndex);
+        for (Cards card: cardsList){
+            cd.getCardDAO().insertCard(card);
+        }
     }
 
     public static List<Decks> getAllDecks(CardsDatabase cd) {
