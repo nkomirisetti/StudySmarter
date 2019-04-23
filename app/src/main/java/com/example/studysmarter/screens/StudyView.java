dabeaa572835112079acc06be8cc74b9bf9ad4b0
Nikhil Komirisetti <nkomirisetti@gmail.com>
2019-04-23 00:23:47 -0400

    created study view screen

---
+++ app/src/main/java/com/example/studysmarter/screens/StudyView.java
@@ -0,0 +1,15 @@
+package com.example.studysmarter.screens;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.studysmarter.R;
+
+public class StudyView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_study_view);
+    }
+}
