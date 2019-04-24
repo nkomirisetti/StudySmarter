82cd0e755e93fae5ae3d088ac7d2b15ea9452590
Nikhil Komirisetti <nkomirisetti@gmail.com>
2019-04-23 22:46:10 -0400

    created an actual activity, not just a java class

--- app/src/main/java/com/example/studysmarter/screens/StudySummary.java
+++
@@ -1,4 +0,0 @@
-package com.example.studysmarter.screens;
-
-public class StudySummary {
-}
---
+++ app/src/main/java/com/example/studysmarter/screens/study_review.java
@@ -0,0 +1,13 @@
+package com.example.studysmarter.screens;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class study_review extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_study_review);
+    }
+}
