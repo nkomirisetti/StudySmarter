d07d4e8639ebfc100302c495f8d1b38796322689
Nikhil Komirisetti <nkomirisetti@gmail.com>
2019-04-24 21:51:27 -0400

    added R reference

--- app/src/main/java/com/example/studysmarter/screens/CalendarScreen.java
+++ app/src/main/java/com/example/studysmarter/screens/CalendarScreen.java
@@ -3,6 +3,8 @@ package com.example.studysmarter.screens;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.studysmarter.R;
+
 public class CalendarScreen extends AppCompatActivity {
 
     @Override
